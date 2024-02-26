b27bc9764db77d493d230a94c63abe41614b6995
Sujata666 <161130520+Sujata666@users.noreply.github.com>
2024-02-26 15:46:41 +0530

    Add files via upload

---
+++ Task2 (1).py
@@ -0,0 +1,11 @@
+'''
+Task 2
+Print Your details:-
+name, age, email.
+'''
+name = "Sujata"
+age = 22
+email = "sujatamaurya2345@gmail.com"
+print("Hello ", name )
+print("Your age is: ", age)
+print("And your email is:", email)
